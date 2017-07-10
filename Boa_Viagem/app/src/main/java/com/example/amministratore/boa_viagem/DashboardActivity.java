47e6f9d8da4be44d47e9092974a8f1636eafb61b
leonardohrjesus <leonardohrjesus@hotmail.com>
2017-07-11 00:39:19 +0200

    adicionandos funcionalidades 1

---
+++ Boa_Viagem/app/src/main/java/com/example/amministratore/boa_viagem/DashboardActivity.java
@@ -0,0 +1,37 @@
+package com.example.amministratore.boa_viagem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by Amministratore on 11/07/2017.
+ */
+
+public class DashboardActivity  extends AppCompatActivity {
+
+    private String opcao;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    setContentView(R.layout.dashboard);
+
+    }
+
+    public void selecionarOpcao(View view) {
+        switch (view.getId()) {
+             case R.id.nova_viagem:
+                 startActivity(new Intent(this, ViagemActivity.class));
+                 TextView textView = (TextView) view;
+                 opcao = "Opção: " + textView.getText().toString();
+                 Toast.makeText(this, opcao, Toast.LENGTH_LONG).show();
+                 break;
+            }
+
+
+    }
+
+}
---
+++ Boa_Viagem/app/src/main/java/com/example/amministratore/boa_viagem/ViagemActivity.java
@@ -0,0 +1,8 @@
+package com.example.amministratore.boa_viagem;
+
+/**
+ * Created by Amministratore on 11/07/2017.
+ */
+
+public class ViagemActivity {
+}
