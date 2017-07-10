d828e15da4d82471de3132bfb0674037c312046b
leonardohrjesus <leonardohrjesus@hotmail.com>
2017-07-11 00:15:47 +0200

    trocando nome layout boa viagem para login

--- Boa_Viagem/app/src/main/java/com/example/amministratore/boa_viagem/BoaViagemActivity.java
+++ Boa_Viagem/app/src/main/java/com/example/amministratore/boa_viagem/BoaViagemActivity.java
@@ -8,6 +8,6 @@ public class BoaViagemActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_boa_viagem);
+        setContentView(R.layout.login);
     }
 }
