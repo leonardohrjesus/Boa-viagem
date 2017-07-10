7fe83235658a65b9c99b2d02afdea9992654d223
leonardohrjesus <leonardohrjesus@hotmail.com>
2017-07-10 23:55:23 +0200

    COMMIT DE INSTALACAO DE AMBIENTE
    LEONARDO

---
+++ Boa_Viagem/app/src/main/java/com/example/amministratore/boa_viagem/BoaViagemActivity.java
@@ -0,0 +1,13 @@
+package com.example.amministratore.boa_viagem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoaViagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boa_viagem);
+    }
+}
