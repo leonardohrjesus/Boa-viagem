c0a771504e92185dcbaab0fb09834b07a20ee0f6
leonardohrjesus <leonardohrjesus@hotmail.com>
2017-07-15 20:37:17 +0200

    tudo correto ate a pag 90

--- Boa_Viagem/app/src/main/java/com/example/amministratore/boa_viagem/GastoListActivity.java
+++ Boa_Viagem/app/src/main/java/com/example/amministratore/boa_viagem/GastoListActivity.java
@@ -1,124 +1,40 @@
+
 package com.example.amministratore.boa_viagem;
+
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-
-/**
- * Created by Amministratore on 12/07/2017.
- */
 
 public class GastoListActivity extends ListActivity implements AdapterView.OnItemClickListener {
-    private String dataAnterior = "";
-
-    private List<Map<String, Object>> gastos;
-
-    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        String[] de = { "data", "descricao", "valor", "categoria" };
-        int[] para = { R.id.data, R.id.descricao,   R.id.valor, R.id.categoria };
-        SimpleAdapter adapter = new SimpleAdapter(this, listarGastos(),       R.layout.lista_gasto, de, para);
-        setListAdapter(adapter);
-      //  lv.setAdapter(adapter);
-        getListView().setOnItemClickListener(this);
-
-     //   lv =(TextView) findViewById(R.id.descricao);
-
-
-        adapter.setViewBinder(new GastoViewBinder());
-
-        // registramos aqui o novo menu de contexto
-        registerForContextMenu(getListView());
-
-    }
-
-
-
-    private List<Map<String, Object>> listarGastos() {
-        gastos = new ArrayList<Map<String, Object>>();
-        Map<String, Object> item = new HashMap<String, Object>();
-        item.put("data", "04/02/2012");
-        item.put("descricao", "Diária Hotel");
-        item.put("valor", "R$ 260,00");
-        item.put("categoria", R.color.categoria_hospedagem);
-        gastos.add(item);
-// pode adicionar mais informações de viagens
-        return gastos;
-
-}
-
-
+        setListAdapter(new ArrayAdapter<String>(this,  android.R.layout.simple_list_item_1, listarGastos()));
+        ListView listView = getListView();
+        listView.setOnItemClickListener(this);
+        }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view,
-                            int position, long id) {
-        Map<String, Object> map = gastos.get(position);
-        String descricao = (String) map.get("descricao");
-        String mensagem = "Gasto selecionada: " + descricao;
-        Toast.makeText(this, mensagem,Toast.LENGTH_SHORT).show();
-    }
-
-    private class GastoViewBinder implements SimpleAdapter.ViewBinder {
-        @Override
-        public boolean setViewValue(View view, Object data,
-                                    String textRepresentation) {
-            if(view.getId() == R.id.data){
-                if(!dataAnterior.equals(data)){
-                    TextView textView = (TextView) view;
-                    textView.setText(textRepresentation);
-                    dataAnterior = textRepresentation;
-                    view.setVisibility(View.VISIBLE);
-                } else {
-                    view.setVisibility(View.GONE);
-                }
-                return true;
-            }
-            if(view.getId() == R.id.categoria){
-                Integer id = (Integer) data;
-                view.setBackgroundColor(getResources().getColor(id));
-                return true;
-            }
-            return false;
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        TextView textView = (TextView) view;
+        Toast.makeText(this,"Gasto selecionado: " + textView.getText(),
+                Toast.LENGTH_SHORT).show();
         }
-    }
 
-
-@Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu,v,menuInfo);
-    //    MenuInflater inflater = getMenuInflater();
-     //   inflater.inflate(R.menu.gasto_menu, menu);
-    }
-@Override
-    public boolean onContextItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.remover) {
-            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item
-                    .getMenuInfo();
-            gastos.remove(info.position);
-            getListView().invalidateViews();
-            dataAnterior = "";
-// remover do banco de dados
-            return true;
+    private List<String> listarGastos() {
+        return Arrays.asList("Sanduíche R$ 19,90",
+                "Táxi Aeroporto - Hotel R$ 34,00",
+                "Revista R$ 12,00");
         }
-        return super.onContextItemSelected(item);
-    }
-}
+     }
--- Boa_Viagem/app/src/main/java/com/example/amministratore/boa_viagem/ViagemListActivity.java
+++ Boa_Viagem/app/src/main/java/com/example/amministratore/boa_viagem/ViagemListActivity.java
@@ -1,192 +1,46 @@
-
 package com.example.amministratore.boa_viagem;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+/**
+ * Created by Amministratore on 14/07/2017.
+ */
+
 import android.app.ListActivity;
-import android.content.DialogInterface;
+
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.view.ContextMenu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.AdapterView;
-import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.SimpleAdapter;
-import android.widget.SimpleAdapter.ViewBinder;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Arrays;
+import java.util.List;
 
-
-
-public class ViagemListActivity extends ListActivity implements AdapterView.OnItemClickListener, DialogInterface.OnClickListener {
-    private List<Map<String, Object>> viagens;
-    private List<Map<String, Object>> gastos;
-    private String dataAnterior = "";
-    private AlertDialog alertDialog;
-    private int viagemSelecionada;
-    private AlertDialog dialogConfirmacao;
-
-
-
+class ViagemListActivity extends ListActivity   implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listarViagens()));
+        ListView listView = getListView();
+        listView.setOnItemClickListener(this);
+    }
+    private List<String> listarViagens() {
+        return Arrays.asList("São Paulo", "Bonito", "Maceió");
+    }
 
-
-        String[] de = { "data", "descricao", "valor", "categoria" };
-        int[] para = { R.id.data, R.id.descricao, R.id.valor, R.id.categoria };
-
-        SimpleAdapter adapter = new SimpleAdapter(this, listarGastos(),
-                R.layout.lista_gasto, de, para);
-
-        adapter.setViewBinder(new GastoViewBinder());
-
-        setListAdapter(adapter);
-        getListView().setOnItemClickListener(this);
-        registerForContextMenu(getListView());
-        this.alertDialog = criaAlertDialog();
-        this.dialogConfirmacao = criaDialogConfirmacao();
-
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+                            long id) {
+        TextView textView = (TextView) view;
+        String mensagem = "Viagem selecionada: " + textView.getText();
+        Toast.makeText(getApplicationContext(), mensagem,
+                Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, GastoListActivity.class));
     }
 
 
-    private List<Map<String, Object>> listarGastos() {
-        gastos = new ArrayList<Map<String, Object>>();
-
-        Map<String, Object> item = new HashMap<String, Object>();
-        item.put("data", "04/02/2012");
-        item.put("descricao", "Diária Hotel");
-        item.put("valor", "R$ 260,00");
-        item.put("categoria", R.color.categoria_hospedagem);
-        gastos.add(item);
-
-        item = new HashMap<String, Object>();
-        item.put("data", "03/02/2012");
-        item.put("descricao", "Wifi");
-        item.put("valor", "R$ 7,00");
-        item.put("categoria", R.color.categoria_outros);
-        gastos.add(item);
-
-        item = new HashMap<String, Object>();
-        item.put("data", "02/02/2012");
-        item.put("descricao", "Táxi Aeroporto - Hotel");
-        item.put("valor", "R$ 34,00");
-        item.put("categoria", R.color.categoria_transporte);
-        gastos.add(item);
-
-        item = new HashMap<String, Object>();
-        item.put("data", "02/02/2012");
-        item.put("descricao", "Sanduíche");
-        item.put("valor", "R$ 19,90");
-        item.put("categoria", R.color.categoria_alimentacao);
-        gastos.add(item);
-
-        return gastos;
-    }
-
-    private class GastoViewBinder implements ViewBinder {
-
-        @Override
-        public boolean setViewValue(View view, Object data,
-                                    String textRepresentation) {
-
-            if (view.getId() == R.id.data) {
-                if (!dataAnterior.equals(data)) {
-                    TextView textView = (TextView) view;
-                    textView.setText(textRepresentation);
-                    dataAnterior = textRepresentation;
-                    view.setVisibility(View.VISIBLE);
-                } else {
-                    view.setVisibility(View.GONE);
-                }
-                return true;
-            }
-
-            if (view.getId() == R.id.categoria) {
-                Integer id = (Integer) data;
-                view.setBackgroundColor(getResources().getColor(id));
-                return true;
-            }
-            return false;
-        }
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v,
-                                    ContextMenuInfo menuInfo) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.gasto_menu, menu);
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-
-        if (item.getItemId() == R.id.remover) {
-            AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
-                    .getMenuInfo();
-            gastos.remove(info.position);
-            getListView().invalidateViews();
-            dataAnterior = "";
-            // remover do banco de dados
-            return true;
-        }
-        return super.onContextItemSelected(item);
-    }
-
-    @Override
-    public void onClick(DialogInterface dialog, int item) {
-        switch (item) {
-            case 0:
-                startActivity(new Intent(this, ViagemActivity.class));
-                break;
-            case 1:
-                startActivity(new Intent(this, GastoActivity.class));
-                break;
-            case 2:
-                startActivity(new Intent(this, GastoListActivity.class));
-                break;
-            case 3:
-                 dialogConfirmacao.show();
-                break;
-            case DialogInterface.BUTTON_POSITIVE:
-                viagens.remove(viagemSelecionada);
-                getListView().invalidateViews();
-                break;
-            case DialogInterface.BUTTON_NEGATIVE:
-                dialogConfirmacao.dismiss();
-                 break;
-             }
-        }
-
-    private AlertDialog criaAlertDialog() {
-        final CharSequence[] items = {
-                getString(R.string.editar),
-                getString(R.string.novo_gasto),
-                getString(R.string.gastos_realizados),
-                getString(R.string.remover) };
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.opcoes);
-        builder.setItems(items, this);
-        return builder.create();
-    }
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position,  long id) {
-        this.viagemSelecionada = position;
-        alertDialog.show();
-
-    }
-
-    private AlertDialog criaDialogConfirmacao() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(R.string.confirmacao_exclusao_viagem);
-        builder.setPositiveButton(getString(R.string.sim),  this);
-        builder.setNegativeButton(getString(R.string.nao), this);
-        return builder.create();
-    }
 }
